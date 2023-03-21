c492687a2a321dfbbcc6c50d28bb2be00c59da67
Svetoslav Yordanov <svetlio@gmail.com>
2023-03-21 12:47:12 +0200

    ScrollTop func

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "react-hook-form";
+import Form from "react-bootstrap/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const methods = useForm({
+    reValidateMode: "onBlur",
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors: formErrors },
+  } = useForm({ mode: "onSubmit" });
+  const radioGroups = 50;
+  const genders = ["Male", "Female", "Non-binary", "Other"];
+
+  const handleErrors = () => {
+    scrollToFirstMissedField();
+  };
+
+  const scrollToFirstMissedField = () => {
+    const firstMissedRequiredField = Object.keys(formErrors)[0];
+    console.log(firstMissedRequiredField);
+    if (firstMissedRequiredField) {
+      const missedFieldRef = register(firstMissedRequiredField);
+      const missedFieldOffsetTop = missedFieldRef;
+
+      if (missedFieldOffsetTop) {
+        console.log(missedFieldOffsetTop);
+        window.scrollTo({
+          top: missedFieldOffsetTop,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ width: 400, margin: "30px auto" }}>
+      <form onSubmit={handleSubmit(handleErrors)}>
+        {[...new Array(radioGroups)].map((_, index) => (
+          <div key={index} style={{ margin: 30 }}>
+            <div>Select Your Gender</div>
+            <div key={index} style={{ background: "#eee", padding: 10 }}>
+              {genders.map((gender) => (
+                <Form.Group key={gender}>
+                  <Form.Check
+                    name={`group${index}`}
+                    type="radio"
+                    label={gender}
+                    {...register(`gender${index}`, {
+                      required: "Gender is required",
+                    })}
+                    isInvalid={!!formErrors.gender}
+                  />
+                </Form.Group>
+              ))}
+              {formErrors[`gender${index}`] && (
+                <div style={{ display: "block" }}>
+                  {formErrors[`gender${index}`].message}!!
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+        <button type="submit">Save</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
